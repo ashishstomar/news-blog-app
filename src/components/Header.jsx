d1db66117073f00ec9b10e3cfa5a58a250b38606
ashishstomar <23213257+ashishstomar@users.noreply.github.com>
2024-07-25 18:52:03 +0530

    fix: routing and styling

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,11 @@
 import React, { useState } from "react";
+import {
+  FaBasketballBall,
+  FaDollarSign,
+  FaInfoCircle,
+  FaRobot,
+} from "react-icons/fa";
+import { MdTrendingUp } from "react-icons/md";
 import { Link } from "react-router-dom";
 
 const Header = () => {
@@ -9,18 +16,21 @@ const Header = () => {
   };
 
   return (
-    <header className="text-center">
-      <section className="py-3 px-10 flex justify-between items-center">
-        <h1 className="font-mono font-extrabold text-red-500 text-4xl">
-          NewsBlog<span className="text-blue-700 text-6xl">.</span>
-        </h1>
+    <header className=" py-3 px-10 text-center md:flex md:flex-row md:justify-between md:items-center lg:flex-row lg:justify-between lg:items-center">
+      <section className="flex justify-between items-center">
+        <Link to="/">
+          <h1 className="font-mono font-extrabold text-red-500 text-4xl">
+            NewsBlog
+            <span className="text-blue-700 text-5xl">.</span>
+          </h1>
+        </Link>
         <button
           className="block md:hidden focus:outline-none"
           onClick={toggleMenu}
         >
           <div className="space-y-2">
             <div
-              className={`w-8 h-0.5 bg-white transform transition duration-300 ${
+              className={`w-8  h-0.5 bg-white transform transition duration-300 ${
                 isOpen ? "rotate-45 translate-y-2" : ""
               }`}
             ></div>
@@ -38,25 +48,40 @@ const Header = () => {
         </button>
       </section>
       <nav
-        className={`flex-col md:flex-row md:flex md:items-center md:gap-3 font-bold font-mono text-gray-300 ${
+        className={`flex-col transition-transform duration-300 ease-in-out md:flex-row md:flex md:items-center md:gap-3 font-bold font-mono text-gray-300 ${
           isOpen ? "flex" : "hidden"
         } md:flex`}
       >
-        <ul className="flex flex-col md:flex-row gap-3 mt-3 md:mt-0">
+        <ul className="flex flex-col md:flex-row gap-4 mt-3 md:mt-0 text-xl items-center">
           <Link to="/trending">
-            <li>Trending</li>
+            <li className="hover:text-red-500 transition duration-300 flex gap-1 items-center">
+              <MdTrendingUp />
+              Trending
+            </li>
           </Link>
           <Link to="technology">
-            <li>Technology</li>
+            <li className="hover:text-red-500 transition duration-300 flex gap-1 items-center">
+              <FaRobot />
+              Technology
+            </li>
           </Link>
           <Link to="/finance">
-            <li>Finance</li>
+            <li className="hover:text-red-500 transition duration-300 flex gap-1 items-center">
+              <FaDollarSign />
+              Finance
+            </li>
           </Link>
           <Link to="/sports">
-            <li>Sports</li>
+            <li className="hover:text-red-500 transition duration-300 flex gap-1 items-center">
+              <FaBasketballBall />
+              Sports
+            </li>
           </Link>
           <Link to="/about">
-            <li>About</li>
+            <li className="hover:text-red-500 transition duration-300 flex gap-1 items-center">
+              <FaInfoCircle />
+              About
+            </li>
           </Link>
         </ul>
       </nav>
