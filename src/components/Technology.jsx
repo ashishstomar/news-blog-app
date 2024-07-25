1c264ddc8cc1f5dfea7186d5eac833888166bcb8
ashishstomar <23213257+ashishstomar@users.noreply.github.com>
2024-07-25 15:02:49 +0530

    add: news post categories

---
+++ src/components/Technology.jsx
@@ -0,0 +1,13 @@
+import BlogPostList from "./BlogPostList";
+
+const Technology = () => {
+  return (
+    <section className="mx-4 my-4 sm:mx-6 sm:my-5 sm:px-4 md:mx-8 md:my-6 md:px-6 lg:mx-12 lg:my-8 lg:px-8">
+      <div>
+        <BlogPostList category="technology" />
+      </div>
+    </section>
+  );
+};
+
+export default Technology;
